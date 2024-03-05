e62f9f28899f85316a73107348325f5f57e5909a
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-03-05 18:05:05 +0200

    fix: some bag fix

--- src/components/common/AdminNavigation.jsx
+++ src/components/common/AdminNavigation.jsx
@@ -1,9 +1,9 @@
-import { ADMIN_DASHBOARD } from '@/constants/routes';
-import logo from '@/images/logo-full.png';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import UserAvatar from '@/views/account/components/UserAvatar';
+import { ADMIN_DASHBOARD } from "@/constants/routes";
+import logo from "@/images/logo-full.png";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import UserAvatar from "@/views/account/components/UserAvatar";
 
 const AdminNavigation = () => {
   const { isAuthenticating, profile } = useSelector((state) => ({
@@ -14,17 +14,17 @@ const AdminNavigation = () => {
   return (
     <nav className="navigation navigation-admin">
       <div className="logo">
-        <Link to={ADMIN_DASHBOARD} style={{ display: 'flex', alignItems: 'center' }}>
+        <Link
+          to={ADMIN_DASHBOARD}
+          style={{ display: "flex", alignItems: "center" }}
+        >
           <img alt="Logo" src={logo} />
           <h3>ADMIN PANEL</h3>
         </Link>
       </div>
       <ul className="navigation-menu">
         <li className="navigation-menu-item">
-          <UserAvatar
-            isAuthenticating={isAuthenticating}
-            profile={profile}
-          />
+          <UserAvatar isAuthenticating={isAuthenticating} profile={profile} />
         </li>
       </ul>
     </nav>
